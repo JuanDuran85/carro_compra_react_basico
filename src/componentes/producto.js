db3bb3ffec0a07bcbe544a8ff0acbbd019b54975
JuanDuran85 <duran85juan@gmail.com>
2019-12-16 23:32:46 -0300

    mostrando el componenete

--- src/componentes/producto.js
+++ src/componentes/producto.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Card from 'reactstrap';
+import {Card} from 'reactstrap';
 
-function producto() {  
+function Producto() {  
     return (
         <Card>
-
+            programando con react js...
         </Card>
     );
 };
+
+export default Producto;
