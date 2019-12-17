44ace1d7348882d9ffc2193b9ae3a391b99bdd3a
JuanDuran85 <duran85juan@gmail.com>
2019-12-17 19:27:46 -0300

    agregando barra de navegacion

---
+++ src/componentes/Navegacion.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Navbar, NavItem, NavLink, NavbarToggler, NavbarBrand, Nav} from 'reactstrap';
+
+class Navegacion extends React.Component {
+    render(){
+        return (
+            <Navbar color="light" light expand="md">
+                <NavbarBrand href="/">{this.props.titulo}</NavbarBrand>
+                <Nav className="ml-auto" navbar>
+                    <NavItem>
+                        <NavLink href="/">Acerca de</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/">Redes Sociales</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/">Contactanos</NavLink>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        );
+    };
+};
+
+export default Navegacion;
