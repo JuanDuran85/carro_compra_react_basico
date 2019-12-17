b3b858a0f2efc1877bfb87e347d91543476a19a9
JuanDuran85 <duran85juan@gmail.com>
2019-12-17 17:54:17 -0300

    llevando a POO el carro del compra

--- src/componentes/producto.js
+++ src/componentes/producto.js
@@ -2,21 +2,24 @@ import React from 'react';
 import {Card, Container, Row, Col, CardImg, CardBody, CardTitle, CardSubtitle, CardText, Button} from 'reactstrap';
 import './producto.css';
 
-function Producto(props) {  
-    return (
-        <Col sm="4">
-            <Card className="Card" body outline color="primary">  
-                <CardImg src={props.imagen}></CardImg>
-                <CardBody>
-                    <CardTitle>{props.titulo}</CardTitle>
-                    <CardSubtitle><b>Precio:</b> {props.precio}</CardSubtitle>
-                    <CardText>{props.descripcion}</CardText>
-                    <Button className="Button">Comprar</Button>
-                    <Button className="Button">Ver Ficha</Button>
-                </CardBody>
-            </Card>
-        </Col>
-    );
+//function Producto (props) { 
+ class Producto extends React.Component{ 
+    render(){   
+        return (    
+            <Col sm="4">
+                <Card className="Card" body outline color="primary">  
+                    <CardImg src={this.props.imagen}></CardImg>
+                    <CardBody>
+                        <CardTitle>{this.props.titulo}</CardTitle>
+                        <CardSubtitle><b>Precio:</b> {this.props.precio}</CardSubtitle>
+                        <CardText>{this.props.descripcion}</CardText>
+                        <Button className="Button">Comprar</Button>
+                        <Button className="Button">Ver Ficha</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    };
 };
 
 export default Producto;
