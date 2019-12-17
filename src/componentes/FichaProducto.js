ea24f4378401c2977d475a0d62cc63ff16baaf73
JuanDuran85 <duran85juan@gmail.com>
2019-12-17 19:05:21 -0300

    agregando mas componentes

---
+++ src/componentes/FichaProducto.js
@@ -0,0 +1,43 @@
+//iniciamos importando react
+import React from 'react';
+import {CardImg, Button, Container, Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import './producto.css';
+
+class FichaProducto extends React.Component {
+    constructor (props) {
+        super();
+        this.state = {
+            modal:false
+        };
+        this.toggle = this.toggle.bind(this);
+    };
+
+    toggle(){
+        console.log(this.props.props);
+        this.setState(prevState => ({
+            modal:!prevState.modal
+        }));
+    };
+
+    render(){
+        return (
+            <Container>
+                <Button onClick={this.toggle}>Ver Ficha</Button>
+                <Modal isOpen={this.state.modal}>
+                    <ModalHeader>{this.props.props.titulo}</ModalHeader>
+                    <ModalBody>
+                        <CardImg src={this.props.props.imagen}></CardImg>
+                        <p>Descripción del producto: {this.props.props.descripcion}.</p>
+                        <p>Precio: {this.props.props.precio}</p>
+                    </ModalBody>
+                    <ModalFooter className="ModalFooter">
+                        <Button color="primary" onClick={this.toggle}>Comprar</Button>
+                        <Button color="secondary" onClick={this.toggle}>Regresar</Button>
+                    </ModalFooter>
+                </Modal>
+            </Container>
+        );
+    };
+};
+
+export default FichaProducto;
